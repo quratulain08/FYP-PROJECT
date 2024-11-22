55bc0e25219f2102b5471812407bc89a311650cb
Ammar Younis <123241157+Ammar9290@users.noreply.github.com>
2024-11-22 16:10:38 +0500

    department,faculity,students in cordinator done

--- my-next-app/src/app/Coordinator/navbar/page.tsx
+++ my-next-app/src/app/Coordinator/navbar/page.tsx
@@ -47,16 +47,16 @@ const Navbar: React.FC = () => {
               <span className="text-sm">Internships</span>
               <span className="text-sm text-gray-700 ml-1"><MdKeyboardArrowDown /></span>
             </Link>
-            <Link href="/Department" className="flex flex-col items-center text-gray-700 hover:text-white hover:bg-green-500 px-3 py-2 rounded">
+            <Link href="/Coordinator/Department" className="flex flex-col items-center text-gray-700 hover:text-white hover:bg-green-500 px-3 py-2 rounded">
               <span className="text-2xl text-red-500 mb-2"><FaFolder /></span>
               <span className="text-sm">Departments & Programs</span>
               <span className="text-sm text-gray-700 ml-1"><MdKeyboardArrowDown /></span>
             </Link>
-            <Link href="/Faculty" className="flex flex-col items-center text-gray-700 hover:text-white hover:bg-green-500 px-3 py-2 rounded">
+            <Link href="/Coordinator/Faculty" className="flex flex-col items-center text-gray-700 hover:text-white hover:bg-green-500 px-3 py-2 rounded">
               <span className="text-2xl text-purple-500 mb-2"><FaChalkboardTeacher /></span>
               <span className="text-sm">Faculty Directory</span>
             </Link>
-            <Link href="/student" className="flex flex-col items-center text-gray-700 hover:text-white hover:bg-green-500 px-3 py-2 rounded">
+            <Link href="/Coordinator/student" className="flex flex-col items-center text-gray-700 hover:text-white hover:bg-green-500 px-3 py-2 rounded">
               <span className="text-2xl text-black mb-2"><FaUsers /></span>
               <span className="text-sm">Students Directory</span>
               <span className="text-sm text-gray-700 ml-1"><MdKeyboardArrowDown /></span>
